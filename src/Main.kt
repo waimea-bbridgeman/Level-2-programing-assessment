f7304f7538e853a1614b9b0811496ddfb2124456
waimea-bbridgeman <brbridgeman@waimea.school.nz>
2025-03-20 12:25:07 +1300

    start

--- src/Main.kt
+++ src/Main.kt
@@ -13,21 +13,55 @@
  * =====================================================================
  */
 
+    const val gameLength = 15
+
+fun setUpGame() {
+    println("Game rules: Old gold is a game for two players that's played on a one-dimensional grid with coins, " +
+            "where the aim is to win by being the player who removes the gold coin.")
+
+    val playerOne = println("Enter player ones username")
+    val playerTwo = println("Enter player twos username")
+
+
+}
+
+
+
 
 
 fun main() {
-    println("")
+    println("Welcome to the Old Gold Coin game!")
+    println("-----------------------------------")
+
+
+    displayGame()
 
 
 
 
+}
+
+fun displayGame() {
+
+    val banner = ("+--------".repeat(gameLength) + "+")
 
 
+    println(banner)
 
+    for (i in 0..<gameSize.size) {
+        print("| Cage ${i + 1} " .padEnd(9))
+    }
+    println("|")
 
+    println(banner)
 
+    for (i in 0..<cageList.size) {
+        print("| ${cageList[i]}" .padEnd(9))
+    }
 
+    println("|")
 
+    println(banner)
 
 }
 
